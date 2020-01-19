c863eb2e4e57aaa127d4751737ddfd58378c1180
Samuel Philip Blackford <sam.p.blackford@gmail.com>
2020-01-19 14:47:24 +0000

    Deleted tests which are no longer applicable. Fixed missing file references. Added notes in for the examiner and for Harry.

--- PatientMonitoringSystem/PatientMonitoringSystem.Tests/ModuleTests.cs
+++ PatientMonitoringSystem/PatientMonitoringSystem.Tests/ModuleTests.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace PatientMonitoringSystem.Tests
 {
 	[TestFixture]
-	public class BloodPressureModuleTests
+	public class ModuleTests // TODO: Be sure to also test the different strategies in different test classes.
 	{
 		[Test]
-		public void TestStrategySet() {
+		public void TestStrategySet() { // TODO: Make this a unit test by mocking out the IModuleStrategy. I can show you how to use a cool library called Moq to do this.
 
 			Module blood_pressure_module = new Module(new BloodPressureStrategy(), 5, 10);
 
 			int test = blood_pressure_module.GetCurrentReading();
 			Console.WriteLine(test);
 		}
+
+		// TODO: Validation hasn't changed. Copy the tests from the old BloodPressureModuleTests found in source control.
 	}
 }
