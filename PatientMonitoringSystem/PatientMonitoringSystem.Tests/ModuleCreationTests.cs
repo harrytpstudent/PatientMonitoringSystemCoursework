2f56e85ade78d48d4f03026246924ef42080b676
harrytpstudent <32169735+harrytpstudent@users.noreply.github.com>
2020-02-02 12:05:17 +0000

    Add unit tests for creation method

---
+++ PatientMonitoringSystem/PatientMonitoringSystem.Tests/ModuleCreationTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PatientMonitoringSystem.Enums;
+using PatientMonitoringSystem.Controllers;
+using PatientMonitoringSystem.Factory;
+
+namespace PatientMonitoringSystem.Tests
+{
+	[TestFixture]
+	class ModuleCreationTests
+	{
+		[TestCase]
+		public void CreateStrategyReturnsBloodPressure() {
+			ModuleFactory module_factory = new ModuleFactory();
+			IModuleStrategy returned_strat = module_factory.CreateStrategy(ModuleType.BloodPressure);
+			Assert.IsInstanceOf(typeof(BloodPressureStrategy), returned_strat);
+		}
+
+		[TestCase]
+		public void CreateStrategyReturnsOxygenLevel()
+		{
+			ModuleFactory module_factory = new ModuleFactory();
+			IModuleStrategy returned_strat = module_factory.CreateStrategy(ModuleType.OxygenLevel);
+			Assert.IsInstanceOf(typeof(OxygenStrategy), returned_strat);
+		}
+
+		[TestCase]
+		public void CreateStrategyReturnsCorrectDefault()
+		{
+			ModuleFactory module_factory = new ModuleFactory();
+			IModuleStrategy returned_strat = module_factory.CreateStrategy((ModuleType)3);
+			Assert.IsInstanceOf(typeof(BloodPressureStrategy), returned_strat);
+		}
+	}
+}
