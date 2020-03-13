d2285468236b34befacb1986fda19bc5c37c0238
Tristan Brown <trizza.b@gmail.com>
2020-03-13 12:39:32 +0000

    Add Login View (does nothing for now) and add a scroll bar for the BedsideSystemRowViews below it

--- PatientMonitoringSystem/PatientMonitoringSystem/Views/ControlSystemView.cs
+++ PatientMonitoringSystem/PatientMonitoringSystem/Views/ControlSystemView.cs
@@ -11,10 +11,12 @@ namespace PatientMonitoringSystem.Views
 {
 	public partial class ControlSystemView : UserControl
 	{
+		TableLayoutPanel bedsideTable;
 		ControlSystemController controlSystemController;
 		Dictionary<Guid, BedsideSystemView> bedside_controller_map = new Dictionary<Guid, BedsideSystemView>();
 		public ControlSystemView(ControlSystemController newControlSystemController)
 		{
+			bedsideTable = new TableLayoutPanel();
 			controlSystemController = newControlSystemController;
 			controlSystemController.NotifyUiAlarmRaised += SignalBedsideAlarm;
 			InitializeComponent();
@@ -34,19 +36,26 @@ namespace PatientMonitoringSystem.Views
 		public void Initialise()
 		{
 			Text = "Control System";
-
 			Table.RowStyles.RemoveAt(0);
 			Table.RowCount = 0;
 
-			List<Guid> bedsideIds = controlSystemController.GetBedsideSystemIds();
+			LoginSystemView loginSystem = new LoginSystemView();
+			Table.RowStyles.Add(new RowStyle());
+			Table.Controls.Add(loginSystem, 0, Table.RowCount - 1);
+
+			bedsideTable.Size = new Size(270, 100);
+			bedsideTable.AutoScroll = true;
+
+			List <Guid> bedsideIds = controlSystemController.GetBedsideSystemIds();
 
 			foreach (var bedsideSystemId in bedsideIds)
 			{
 				AddBedsideSystem(bedsideSystemId);
 			}
-
 			ViewBedsideSystem(controlSystemController.GetFirstBedsideSystem());
 
+			bedsideTable.Dock = DockStyle.Fill;
+			Table.Controls.Add(bedsideTable);
 			Dock = DockStyle.Fill;
 		}
 
@@ -58,8 +67,8 @@ namespace PatientMonitoringSystem.Views
 			BedsideSystemController bedsideSystemController = new BedsideSystemController(bedside);
 			bedside_controller_map.Add(bedside.BedsideSystemId, new BedsideSystemView(bedsideSystemController));
 
-			Table.RowStyles.Add(new RowStyle());
-			Table.Controls.Add(bedsideSystemRowView, 0, Table.RowCount - 1);
+			bedsideTable.RowStyles.Add(new RowStyle());
+			bedsideTable.Controls.Add(bedsideSystemRowView, 0, Table.RowCount - 1);
 		}
 
 		public void ViewBedsideSystem(Guid bedsideSystemId)
@@ -79,7 +88,7 @@ namespace PatientMonitoringSystem.Views
 
 		public void SignalBedsideAlarm(object sender, Guid bedsideId)
 		{
-			foreach (BedsideSystemRowView bedsideRowView in Table.Controls) {
+			foreach (BedsideSystemRowView bedsideRowView in bedsideTable.Controls) {
 				if (bedsideRowView.BedsideSystemId == bedsideId) {
 					bedsideRowView.BackColor = Color.Red;
 				}
